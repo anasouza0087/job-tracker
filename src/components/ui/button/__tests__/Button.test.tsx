4c9ce3f973dc19a112bea51198d189c323b3ad8b
ana <ana.souza0087@gmail.com>
2025-05-18 16:52:01 -0300

    test: add button tests

--- src/components/ui/button/__tests__/Button.test.tsx
+++ src/components/ui/button/__tests__/Button.test.tsx
@@ -16,4 +16,26 @@ describe("button suite tests", () => {
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
   });
+
+  it("should check the color displayed for the variant primary", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Button variant={"primary"} label={"Click me"} onClick={vi.fn} />
+      </ThemeProvider>
+    );
+
+    const button = screen.getByRole("button");
+    expect(button).toHaveStyle("background-color: #4f46e5");
+  });
+
+  it("should check the color displayed for the variant secondary", () => {
+    render(
+      <ThemeProvider theme={theme}>
+        <Button variant={"secondary"} label={"Click me"} onClick={vi.fn} />
+      </ThemeProvider>
+    );
+
+    const button = screen.getByRole("button");
+    expect(button).toHaveStyle("background-color: #06b6d4");
+  });
 });
